f6afcd6592ff96d74c322072f58ca6da0d0ab6b0
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-24 06:22:30 -0700

    Craete splash controller and add check authed method

--- lib/BussinessLayer/Controllers/splash_controller.dart
+++ lib/BussinessLayer/Controllers/splash_controller.dart
@@ -1,3 +1,15 @@
+import 'package:captin_app/Constants/get_routes.dart';
 import 'package:get/get.dart';
 
-class SplashController extends GetxController {}
+class SplashController extends GetxController {
+  Future<void> checkAuthed() async {
+    Future.delayed(const Duration(seconds: 3))
+        .then((value) => Get.offAndToNamed(AppRoutes.loginScreen));
+  }
+
+  @override
+  void onInit() {
+    checkAuthed();
+    super.onInit();
+  }
+}
