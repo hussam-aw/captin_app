0fd9a80b390cab65bd0374c8e27669755e2683b8
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-24 06:23:08 -0700

    Create login controller

--- lib/PresentationLayer/Public/login_screen.dart
+++ lib/PresentationLayer/Public/login_screen.dart
@@ -1,3 +1,9 @@
+import 'package:captin_app/PresentationLayer/Widgets/Public/accept_button.dart';
+import 'package:captin_app/PresentationLayer/Widgets/Public/app_icon_header.dart';
+import 'package:captin_app/PresentationLayer/Widgets/Public/ord_text_form_field.dart';
+import 'package:captin_app/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:captin_app/PresentationLayer/Widgets/Public/primary_line.dart';
+import 'package:captin_app/PresentationLayer/Widgets/Public/spacer_height.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -5,6 +11,51 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 80),
+            child: Column(
+              children: [
+                const AppIconHeader(),
+                const PageTitle(
+                  alignment: Alignment.center,
+                  title: 'تسجيل الدخول',
+                ),
+                spacerHeight(height: 22),
+                Expanded(
+                  flex: 3,
+                  child: Form(
+                    child: Column(
+                      children: [
+                        OrdTextFormField(
+                          keyboardType: TextInputType.phone,
+                          hintText: 'رقم الموبايل',
+                        ),
+                        spacerHeight(height: 22),
+                        OrdTextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          hintText: 'كلمة المرور',
+                        ),
+                        spacerHeight(height: 22),
+                        AcceptButton(
+                          onPressed: () {},
+                          text: 'المتابعة',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomSheet: const PrimaryLine(),
+      ),
+    );
   }
 }
