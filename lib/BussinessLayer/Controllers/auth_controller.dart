a048a27f33307cfa2bc6e4a83defff028e3f520f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-26 04:59:27 -0700

    Create auth controller and add login and logout method

---
+++ lib/BussinessLayer/Controllers/auth_controller.dart
@@ -0,0 +1,43 @@
+import 'package:captin_app/Constants/get_routes.dart';
+import 'package:captin_app/DataAccesslayer/Clients/box_client.dart';
+import 'package:captin_app/DataAccesslayer/Models/user.dart';
+import 'package:captin_app/DataAccesslayer/Repositories/user_repo.dart';
+import 'package:captin_app/PresentationLayer/Widgets/Public/snackbars.dart';
+import 'package:captin_app/main.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  UserRepo userRepo = UserRepo();
+  BoxClient boxClient = BoxClient();
+  var logging = false.obs;
+
+  Future<void> login() async {
+    logging.value = true;
+    if (phoneNumberController.value.text.isNotEmpty &&
+        passwordController.value.text.isNotEmpty) {
+      User? user = await userRepo.login(
+          phoneNumberController.value.text, passwordController.value.text);
+
+      if (user != null) {
+        MyApp.appUser = user;
+        await boxClient.setAuthedUser(user);
+        SnackBars.showSuccess("${'أهلاً بك'}${user.name}");
+        Get.toNamed(AppRoutes.ordersScreen);
+      } else {
+        SnackBars.showWarning('بياناتك لا تتطابق مع سجلاتنا');
+      }
+    } else {
+      SnackBars.showWarning('يرجى تعبئة الحقول المطلوبة للمتابعة');
+    }
+    logging.value = false;
+  }
+
+  Future<void> logout() async {
+    await boxClient.clearStorage();
+    MyApp.appUser = null;
+    Get.offAllNamed(AppRoutes.loginScreen);
+  }
+}
