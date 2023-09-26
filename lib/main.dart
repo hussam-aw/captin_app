23c206df714c625903e0d97ab7ea828afa2d5d67
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-26 08:06:53 -0700

    Initialize get storage

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:captin_app/Constants/ui_themes.dart';
 import 'package:captin_app/DataAccesslayer/Models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
