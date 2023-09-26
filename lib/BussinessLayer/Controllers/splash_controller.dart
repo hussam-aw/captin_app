74ebc1a8dcbf68c3dab35fbf3e753ba13121bc9c
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-26 08:09:38 -0700

    Update check authed method to save logged user

--- lib/BussinessLayer/Controllers/splash_controller.dart
+++ lib/BussinessLayer/Controllers/splash_controller.dart
@@ -1,10 +1,21 @@
 import 'package:captin_app/Constants/get_routes.dart';
+import 'package:captin_app/DataAccesslayer/Clients/box_client.dart';
+import 'package:captin_app/main.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
+  BoxClient boxClient = BoxClient();
+
   Future<void> checkAuthed() async {
-    Future.delayed(const Duration(seconds: 3))
-        .then((value) => Get.offAndToNamed(AppRoutes.loginScreen));
+    bool authed = await boxClient.getAuthState();
+    if (authed == true) {
+      MyApp.appUser = await boxClient.getAuthedUser();
+      Future.delayed(const Duration(seconds: 3))
+          .then((value) => Get.offAndToNamed(AppRoutes.ordersScreen));
+    } else {
+      Future.delayed(const Duration(seconds: 3))
+          .then((value) => Get.offAndToNamed(AppRoutes.loginScreen));
+    }
   }
 
   @override
