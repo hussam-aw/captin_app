21d8771a810c0c829b099f28cab29a2f71a57864
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-26 04:59:04 -0700

    Add login method to client and repo

--- lib/DataAccesslayer/Clients/user_client.dart
+++ lib/DataAccesslayer/Clients/user_client.dart
@@ -1,3 +1,21 @@
-//import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class UserClient {}
+import 'package:captin_app/Constants/api_links.dart';
+import 'package:http/http.dart' as http;
+
+class UserClient {
+  Future<dynamic> login(phone, password) async {
+    var response = await http.post(Uri.parse("$baseUrl$loginLink"),
+        body:
+            jsonEncode(<String, dynamic>{"phone": phone, "password": password}),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return null;
+    }
+  }
+}
--- lib/DataAccesslayer/Repositories/user_repo.dart
+++ lib/DataAccesslayer/Repositories/user_repo.dart
@@ -1,5 +1,16 @@
+import 'dart:convert';
+
 import 'package:captin_app/DataAccesslayer/Clients/user_client.dart';
+import 'package:captin_app/DataAccesslayer/Models/user.dart';
 
 class UserRepo {
   UserClient client = UserClient();
+
+  Future<User?> login(phone, password) async {
+    var data = await client.login(phone, password);
+    if (data != null) {
+      return User.fromApiJson(jsonDecode(data));
+    }
+    return null;
+  }
 }
