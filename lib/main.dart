73e3a7c4516a4df320023c2f51437bce90beb755
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-26 04:58:11 -0700

    Add appUser variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:captin_app/Constants/get_pages.dart';
 import 'package:captin_app/Constants/ui_themes.dart';
+import 'package:captin_app/DataAccesslayer/Models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,6 +10,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  static User? appUser;
 
   @override
   Widget build(BuildContext context) {
