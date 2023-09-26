2d21794fd6b20b2f6b1f8ffa91d1d8536cfb844b
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-26 04:57:50 -0700

    Create box client to save and resore saved user

---
+++ lib/DataAccesslayer/Clients/box_client.dart
@@ -0,0 +1,30 @@
+import 'package:get_storage/get_storage.dart';
+import '../Models/user.dart';
+
+class BoxClient {
+  var box = GetStorage();
+  Future<bool> getAuthState() async {
+    if (await box.read('ord_captainy_authed') != null) {
+      return true;
+    }
+    return false;
+  }
+
+  Future<User> getAuthedUser() async {
+    return User.fromJson(await box.read('ord_captain_userdata'));
+  }
+
+  Future<void> setAuthedUser(User user) async {
+    await box.write('ord_captainy_authed', true);
+    await box.write('ord_captain_userdata', user.toJson());
+  }
+
+  Future<void> removeUserData() async {
+    await box.remove('ord_captainy_authed');
+    await box.remove('ord_captain_userdata');
+  }
+
+  Future<void> clearStorage() async {
+    await removeUserData();
+  }
+}
