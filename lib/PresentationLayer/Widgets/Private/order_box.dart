d56cff9ddd6f0ab2a42ff2aeacdf2feee320434f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-27 06:26:07 -0700

    Update order box for navigation to order info screen

--- lib/PresentationLayer/Widgets/Private/order_box.dart
+++ lib/PresentationLayer/Widgets/Private/order_box.dart
@@ -6,60 +6,64 @@ import 'package:captin_app/PresentationLayer/Widgets/Public/spacer_height.dart';
 import 'package:flutter/material.dart';
 
 class OrderBox extends StatelessWidget {
-  const OrderBox({super.key, required this.order, this.editOnPressed});
+  const OrderBox({super.key, required this.order, required this.onTap});
 
   final Order order;
-  final Function()? editOnPressed;
+  final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-      decoration: const BoxDecoration(
-        color: UIColors.lightDeepBlue,
-        borderRadius: raduis20,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              height: 67,
-              decoration: const BoxDecoration(
-                color: UIColors.red,
-                borderRadius: raduis20,
-              ),
-              child: Center(
-                child: Text(
-                  '#${order.id}',
-                  softWrap: true,
-                  style: UITextStyle.heading,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+        decoration: const BoxDecoration(
+          color: UIColors.lightDeepBlue,
+          borderRadius: raduis20,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: 67,
+                decoration: const BoxDecoration(
+                  color: UIColors.red,
+                  borderRadius: raduis20,
+                ),
+                child: Center(
+                  child: Text(
+                    '#${order.id}',
+                    softWrap: true,
+                    style: UITextStyle.heading,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'حالة الطلب: قيد التوصيل',
-                    style: UITextStyle.body,
-                  ),
-                  spacerHeight(height: 12),
-                  Text(
-                    'اجمالي المبلغ: ${order.total}',
-                    style: UITextStyle.xsmall,
-                  ),
-                ],
+            Expanded(
+              flex: 3,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'حالة الطلب: قيد التوصيل',
+                      style: UITextStyle.body,
+                    ),
+                    spacerHeight(height: 12),
+                    Text(
+                      'اجمالي المبلغ: ${order.total}',
+                      style: UITextStyle.xsmall,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
