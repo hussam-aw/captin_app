373e3835eefb761fb2c7d983e42ef02060e167c3
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-27 06:28:29 -0700

    Add meal name and price properties

--- lib/DataAccesslayer/Models/order_item.dart
+++ lib/DataAccesslayer/Models/order_item.dart
@@ -1,26 +1,34 @@
 class OrderItem {
   int mealId;
+  String mealName;
   int qty;
+  num price;
   num total;
   String? specialOrder;
 
   OrderItem({
     required this.mealId,
+    required this.mealName,
     required this.qty,
+    required this.price,
     required this.total,
     required this.specialOrder,
   });
 
   factory OrderItem.fromJson(Map<String, dynamic> json) => OrderItem(
-        mealId: json["product_id"],
+        mealId: json["id"],
+        mealName: json["meal"],
         qty: json["quantity"],
+        price: json["price"],
         total: json['total_price'],
         specialOrder: json["special_order"] ?? "",
       );
 
   Map<String, dynamic> toJson() => {
-        "product_id": mealId,
+        "id": mealId,
+        "meal": mealName,
         "quantity": qty,
+        "price": price,
         "total_price": total,
         "special_order": specialOrder,
       };
