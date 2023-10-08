87da02c234be8925a6c084121782b351b3ac1e56
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-08 14:58:35 -0700

    Add meals link

--- lib/Constants/api_links.dart
+++ lib/Constants/api_links.dart
@@ -1,5 +1,6 @@
 const baseUrl = "https://rowadhalab.brain.sy/api/v1";
 const loginLink = "/login-captain";
+const mealsLink = "/meals";
 const ordersLink = "/ordersByCaptain";
 const orderStatesLink = "/status";
 const editStatusOrderLink = "/editStatusOrder";
