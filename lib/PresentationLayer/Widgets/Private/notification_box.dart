a39355798c9546043c8447b18ede61cca3c6dc99
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-12 06:53:28 -0700

    Create notifications box widget

---
+++ lib/PresentationLayer/Widgets/Private/notification_box.dart
@@ -0,0 +1,57 @@
+import 'package:captin_app/BussinessLayer/Helpers/date_helper.dart';
+import 'package:captin_app/Constants/ui_colors.dart';
+import 'package:captin_app/Constants/ui_styles.dart';
+import 'package:captin_app/Constants/ui_text_styles.dart';
+import 'package:captin_app/DataAccesslayer/Models/user_notification.dart';
+import 'package:captin_app/PresentationLayer/Widgets/Public/spacer_height.dart';
+import 'package:flutter/material.dart';
+
+class NotificationBox extends StatelessWidget {
+  const NotificationBox({super.key, required this.notification});
+
+  final UserNotification notification;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: raduis22,
+        color: UIColors.lightDeepBlue,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      child: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                notification.title,
+                style: UITextStyle.title,
+              ),
+              spacerHeight(),
+              Text(
+                notification.subtitle,
+                style: UITextStyle.medium,
+              ),
+              spacerHeight(),
+              Text(
+                notification.date,
+                style: UITextStyle.body.copyWith(
+                  color: UIColors.lightGrey,
+                ),
+              ),
+              spacerHeight(),
+              Text(
+                DateHelper.getDateString(notification.createdAt),
+                style: UITextStyle.body.copyWith(
+                  color: UIColors.lightGrey,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
