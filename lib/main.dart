7bd210fd7d6410006c6ef9b05f47753528734cba
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-12 06:51:33 -0700

    Initialize firease and local notification and add new public variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:captin_app/BussinessLayer/Helpers/local_notification_helper.dart';
 import 'package:captin_app/Constants/get_pages.dart';
 import 'package:captin_app/Constants/ui_themes.dart';
 import 'package:captin_app/DataAccesslayer/Models/user.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await GetStorage.init();
+  await LocalNotificationHelper.initialize();
   runApp(const MyApp());
 }
 
@@ -14,6 +19,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   static User? appUser;
+  static bool isAvailable = false;
+  static int bottomSelectedItem = 0;
 
   @override
   Widget build(BuildContext context) {
