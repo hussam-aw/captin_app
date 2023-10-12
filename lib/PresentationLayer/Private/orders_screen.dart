f478eb453173ad691e2917ce8bddc2c6d94f630a
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-12 06:56:50 -0700

    Use user controller to update captain availability and refactor screen code

--- lib/PresentationLayer/Private/orders_screen.dart
+++ lib/PresentationLayer/Private/orders_screen.dart
@@ -1,13 +1,16 @@
 import 'package:captin_app/BussinessLayer/Controllers/orders_controller.dart';
+import 'package:captin_app/BussinessLayer/Controllers/user_controller.dart';
 import 'package:captin_app/Constants/get_routes.dart';
 import 'package:captin_app/Constants/ui_text_styles.dart';
 import 'package:captin_app/PresentationLayer/Widgets/Private/order_box.dart';
 import 'package:captin_app/PresentationLayer/Widgets/Public/loading_item.dart';
 import 'package:captin_app/PresentationLayer/Widgets/Public/ord_appbar.dart';
+import 'package:captin_app/PresentationLayer/Widgets/Public/ord_bottom_navigation_bar.dart';
 import 'package:captin_app/PresentationLayer/Widgets/Public/ord_drawer.dart';
 import 'package:captin_app/PresentationLayer/Widgets/Public/page_title.dart';
 import 'package:captin_app/PresentationLayer/Widgets/Public/spacer_height.dart';
 import 'package:captin_app/PresentationLayer/Widgets/Public/spacer_width.dart';
+import 'package:captin_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,6 +18,7 @@ class OrdersScreen extends StatelessWidget {
   OrdersScreen({super.key});
 
   final ordersController = Get.put(OrdersController());
+  final userController = Get.put(UserController());
 
   Widget buildOrdersList(orders) {
     return orders.isEmpty
@@ -53,14 +57,16 @@ class OrdersScreen extends StatelessWidget {
             children: [
               Row(
                 children: [
-                  Obx(() {
-                    return Switch(
-                      value: !ordersController.ordersReceivingState.value,
-                      onChanged: (value) {
-                        ordersController.toggleOrdersReceivingState();
-                      },
-                    );
-                  }),
+                  GetBuilder<UserController>(
+                      init: userController,
+                      builder: (context) {
+                        return Switch(
+                          value: !MyApp.isAvailable,
+                          onChanged: (value) {
+                            userController.updateCaptainAvailability(!value);
+                          },
+                        );
+                      }),
                   spacerWidth(),
                   const Text(
                     'غير جاهز لاستلام الطلبات',
@@ -71,24 +77,18 @@ class OrdersScreen extends StatelessWidget {
               const PageTitle(title: 'الطلبات'),
               spacerHeight(),
               Expanded(
-                child: Obx(
-                  () {
-                    return ordersController.ordersReceivingState.value
-                        ? RefreshIndicator(
-                            onRefresh: () => ordersController.getOrders(),
-                            child: ordersController.isLoadingOrders.value
-                                ? Center(
-                                    child:
-                                        loadingItem(width: 100, isWhite: true))
-                                : buildOrdersList(ordersController.orders),
-                          )
-                        : Container();
-                  },
-                ),
-              ),
+                  child: RefreshIndicator(
+                onRefresh: () => ordersController.getOrders(),
+                child: Obx(() {
+                  return ordersController.isLoadingOrders.value
+                      ? Center(child: loadingItem(width: 100, isWhite: true))
+                      : buildOrdersList(ordersController.orders);
+                }),
+              )),
             ],
           ),
         ),
+        bottomNavigationBar: const OrdBottomNavigationBar(),
       ),
     );
   }
