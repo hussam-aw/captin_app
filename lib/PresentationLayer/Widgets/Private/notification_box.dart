71b712686a285f2c9909c4479fbd5dd118244046
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-14 05:26:13 -0700

    Add inkwell widget for navigation

--- lib/PresentationLayer/Widgets/Private/notification_box.dart
+++ lib/PresentationLayer/Widgets/Private/notification_box.dart
@@ -7,50 +7,58 @@ import 'package:captin_app/PresentationLayer/Widgets/Public/spacer_height.dart';
 import 'package:flutter/material.dart';
 
 class NotificationBox extends StatelessWidget {
-  const NotificationBox({super.key, required this.notification});
+  const NotificationBox({
+    super.key,
+    required this.notification,
+    required this.onTap,
+  });
 
   final UserNotification notification;
+  final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        borderRadius: raduis22,
-        color: UIColors.lightDeepBlue,
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-      child: Row(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                notification.title,
-                style: UITextStyle.title,
-              ),
-              spacerHeight(),
-              Text(
-                notification.subtitle,
-                style: UITextStyle.medium,
-              ),
-              spacerHeight(),
-              Text(
-                notification.date,
-                style: UITextStyle.body.copyWith(
-                  color: UIColors.lightGrey,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: raduis22,
+          color: UIColors.lightDeepBlue,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  notification.title,
+                  style: UITextStyle.title,
                 ),
-              ),
-              spacerHeight(),
-              Text(
-                DateHelper.getDateString(notification.createdAt),
-                style: UITextStyle.body.copyWith(
-                  color: UIColors.lightGrey,
+                spacerHeight(),
+                Text(
+                  notification.subtitle,
+                  style: UITextStyle.medium,
                 ),
-              ),
-            ],
-          )
-        ],
+                spacerHeight(),
+                Text(
+                  notification.date,
+                  style: UITextStyle.body.copyWith(
+                    color: UIColors.lightGrey,
+                  ),
+                ),
+                spacerHeight(),
+                Text(
+                  DateHelper.getDateString(notification.createdAt),
+                  style: UITextStyle.body.copyWith(
+                    color: UIColors.lightGrey,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
